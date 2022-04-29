209903b0a1e8888a2500607882c3edb84fbc32f0
welves de paula <welvesdepaula@gmail.com>
2022-04-29 16:58:23 -0300

    dev update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('login', 'AuthController@store');
+Route::post('logout', 'AuthController@destroy');
+Route::get('tets', 'AuthController@show');
+
+Route::middleware(['appAuth'])->group(
+    function () {
+        Route::get('user', 'AppController@index');
+    }
+);
+
+
+
+
+
+
+
+Route::get('{any?}', 'SpaController@index')->where('any', '.*');;
